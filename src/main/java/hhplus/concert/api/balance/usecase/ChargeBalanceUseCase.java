ee5ecc2d9fe9572417c89019d2779c5026860565
WonhoLeeDev <wonholee.dev@gmail.com>
2024-04-30 00:40:15 +0900

    ♻️Add saveBalanceHistory process to ChargeBalanceUseCase

--- src/main/java/hhplus/concert/api/balance/usecase/ChargeBalanceUseCase.java
+++ src/main/java/hhplus/concert/api/balance/usecase/ChargeBalanceUseCase.java
@@ -1,6 +1,7 @@
 package hhplus.concert.api.balance.usecase;
 
 import hhplus.concert.api.balance.dto.response.BalanceChargeResponse;
+import hhplus.concert.domain.balance.components.BalanceHistoryWriter;
 import hhplus.concert.domain.user.components.UserReader;
 import hhplus.concert.domain.user.models.User;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static hhplus.concert.api.common.ResponseResult.SUCCESS;
+import static hhplus.concert.domain.balance.models.TransactionType.CHARGE;
 
 @Service
 @RequiredArgsConstructor
@@ -15,10 +17,13 @@ import static hhplus.concert.api.common.ResponseResult.SUCCESS;
 public class ChargeBalanceUseCase {
 
     private final UserReader userReader;
+    private final BalanceHistoryWriter balanceHistoryWriter;
 
     public BalanceChargeResponse excute(Long userId, long amount) {
         User user = userReader.getUserById(userId);
         user.chargeBalance(amount);
+
+        balanceHistoryWriter.saveBalanceUseHistory(user, amount, CHARGE);
         return BalanceChargeResponse.from(SUCCESS, user.getBalance());
     }
 }
