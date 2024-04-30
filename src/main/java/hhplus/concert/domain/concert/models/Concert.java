d73366d9f2f2917ebf4c6541c29be6ccc1521938
WonhoLeeDev <wonholee.dev@gmail.com>
2024-04-30 18:16:48 +0900

    :recycle: Add Concert Builder

--- src/main/java/hhplus/concert/domain/concert/models/Concert.java
+++ src/main/java/hhplus/concert/domain/concert/models/Concert.java
@@ -1,13 +1,17 @@
 package hhplus.concert.domain.concert.models;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "concert")
 public class Concert {
 
@@ -21,4 +25,12 @@ public class Concert {
 
     @OneToMany(mappedBy = "concert")
     private List<ConcertOption> concertOptions = new ArrayList<>();
+
+    @Builder
+    private Concert(Long id, String title, String organizer, List<ConcertOption> concertOptions) {
+        this.id = id;
+        this.title = title;
+        this.organizer = organizer;
+        this.concertOptions = concertOptions;
+    }
 }
