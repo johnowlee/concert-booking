b8da2840a8f5d1e4855a1cf8daa19454ce5006b0
WonhoLeeDev <wonholee.dev@gmail.com>
2024-05-07 14:22:33 +0900

    :rotating_light: 유저조회 테스트

---
+++ src/test/java/hhplus/concert/domain/user/components/UserReaderTest.java
@@ -0,0 +1,47 @@
+package hhplus.concert.domain.user.components;
+
+import hhplus.concert.domain.user.models.User;
+import hhplus.concert.domain.user.repositories.UserReaderRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class UserReaderTest {
+
+    @InjectMocks
+    UserReader userReader;
+
+    @Mock
+    UserReaderRepository userReaderRepository;
+
+    @DisplayName("인자값이 모두 유효하면, 유저 조회에 성공한다.")
+    @Test
+    void getUserById_Success_ifWithValidArguments() {
+        // given
+        Long userId = 1L;
+        String name = "John Doe";
+        User expected = User.builder()
+                .id(userId)
+                .name(name)
+                .build();
+
+        given(userReaderRepository.getUserById(userId)).willReturn(expected);
+
+        // when
+        User result = userReader.getUserById(userId);
+
+        // then
+        assertThat(result.getId()).isEqualTo(expected.getId());
+        assertThat(result.getName()).isEqualTo(expected.getName());
+        verify(userReaderRepository, times(1)).getUserById(userId);
+    }
+}
