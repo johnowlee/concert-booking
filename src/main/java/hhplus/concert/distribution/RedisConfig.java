14340e96e3fdb91822e20755ed79ce0ced4bac94
WonhoLeeDev <wonholee.dev@gmail.com>
2024-05-08 23:32:27 +0900

    :wrench: Add RedisMessageListener

--- src/main/java/hhplus/concert/distribution/RedisConfig.java
+++ src/main/java/hhplus/concert/distribution/RedisConfig.java
@@ -1,2 +1,17 @@
-package hhplus.concert.distribution;public class RedisConfig {
+package hhplus.concert.distribution;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        return container;
+    }
 }
