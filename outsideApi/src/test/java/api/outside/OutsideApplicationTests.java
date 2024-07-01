40b76e1e184d5cf21bce8b2d08e9ac67fc96ab67
WonhoLeeDev <wonholee.dev@gmail.com>
2024-07-02 00:22:34 +0900

    :sparkles: 외부 Kafka Consumer 모듈 추가

---
+++ outsideApi/src/test/java/api/outside/OutsideApplicationTests.java
@@ -0,0 +1,13 @@
+package api.outside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutsideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
