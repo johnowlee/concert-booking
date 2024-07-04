9e32a804c02faaa756d93783ef1e012f9a6b4711
WonhoLeeDev <wonholee.dev@gmail.com>
2024-07-04 13:09:25 +0900

    :recycle: 예약목록조회API response 수정

--- src/main/java/hhplus/concert/api/booking/dto/response/bookings/BookingsResponse.java
+++ src/main/java/hhplus/concert/api/booking/dto/response/bookings/BookingsResponse.java
@@ -1,34 +1,26 @@
 package hhplus.concert.api.booking.dto.response.bookings;
 
 import hhplus.concert.domain.booking.models.Booking;
-import hhplus.concert.domain.concert.models.Seat;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static hhplus.concert.api.booking.dto.response.BookingCommonUtil.*;
-
 public record BookingsResponse(List<BookingsDto> bookings) {
 
     public static BookingsResponse from(List<Booking> bookings) {
-        return new BookingsResponse(setBookingDtos(bookings));
+        return new BookingsResponse(convertBookingsDtoList(bookings));
     }
 
-    private static List<BookingsDto> setBookingDtos(List<Booking> bookings) {
+    private static List<BookingsDto> convertBookingsDtoList(List<Booking> bookings) {
         return bookings.stream()
-                .map(b -> {
-                    List<Seat> seats = getSeatsFromBooking(b);
-                    return BookingsDto.of(
-                            b.getId(),
-                            b.getBookingStatus(),
-                            b.getBookingDateTime(),
-                            b.getConcertTitle(),
-                            getConcertOptionFromSeats(seats).getConcertDateTime(),
-                            getSeatNoFromSeats(seats)
-                    );
-                })
+                .map(b -> toBookingsDto(b))
                 .collect(Collectors.toList());
     }
 
-
+    private static BookingsDto toBookingsDto(Booking booking) {
+        return BookingsDto.of(booking.getId(),
+                booking.getBookingStatus(),
+                booking.getBookingDateTime(),
+                booking.getConcertTitle());
+    }
 }
