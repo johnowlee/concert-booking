337b9ad2bcf09f5fcb8b2244b738e238bd6b8029
WonhoLeeDev <wonholee.dev@gmail.com>
2024-07-04 13:17:56 +0900

    :recycle: BookingConcertDto 리팩토링

--- src/main/java/hhplus/concert/api/booking/dto/response/booking/BookingConcertDto.java
+++ src/main/java/hhplus/concert/api/booking/dto/response/booking/BookingConcertDto.java
@@ -1,6 +1,7 @@
 package hhplus.concert.api.booking.dto.response.booking;
 
 import hhplus.concert.domain.booking.models.Booking;
+import hhplus.concert.domain.booking.models.BookingSeat;
 import hhplus.concert.domain.concert.models.ConcertOption;
 import hhplus.concert.domain.concert.models.Seat;
 
@@ -8,16 +9,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static hhplus.concert.api.booking.dto.response.BookingCommonUtil.getConcertOptionFromSeats;
-import static hhplus.concert.api.booking.dto.response.BookingCommonUtil.getSeatsFromBooking;
-
 public record BookingConcertDto(Long concertOptionId,
                                 String title,
                                 LocalDateTime concertDateTime,
                                 List<BookingSeatDto> seats) {
+
     public static BookingConcertDto from(Booking booking) {
         List<Seat> seats = getSeatsFromBooking(booking);
         ConcertOption concertOption = getConcertOptionFromSeats(seats);
+
         return new BookingConcertDto(
                 concertOption.getId(),
                 booking.getConcertTitle(),
@@ -28,7 +28,17 @@ public record BookingConcertDto(Long concertOptionId,
 
     private static List<BookingSeatDto> toBookingSeatDtos(List<Seat> seats) {
         return seats.stream()
-                .map(s -> BookingSeatDto.from(s))
+                .map(BookingSeatDto::from)
                 .collect(Collectors.toList());
+    }
+
+    private static List<Seat> getSeatsFromBooking(Booking booking) {
+        return booking.getBookingSeats().stream()
+                .map(BookingSeat::getSeat)
+                .collect(Collectors.toList());
+    }
+
+    private static ConcertOption getConcertOptionFromSeats(List<Seat> seats) {
+        return seats.get(0).getConcertOption();
     }
 }
