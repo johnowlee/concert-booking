e36a593bcf5312f5b648e4cf058316b84f52b184
WonhoLeeDev <wonholee.dev@gmail.com>
2024-07-08 00:41:28 +0900

    :coffin: QueueResponse 불필요 생성메서드 삭제

--- src/main/java/hhplus/concert/api/queue/dto/response/QueueResponse.java
+++ src/main/java/hhplus/concert/api/queue/dto/response/QueueResponse.java
@@ -1,22 +1,12 @@
 package hhplus.concert.api.queue.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import hhplus.concert.domain.queue.model.Key;
 import hhplus.concert.domain.queue.model.Queue;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record QueueResponse(String token, String key, Long waitingNumber) {
 
-    public static QueueResponse createActiveQueueResponse(String token) {
-        return new QueueResponse(token, Key.ACTIVE.toString(), null);
-    }
-
-    public static QueueResponse createWaitingQueueResponse(String token, Long waitingNumber) {
-        return new QueueResponse(token, Key.WAITING.toString(), waitingNumber);
-    }
-
     public static QueueResponse createQueueResponse(Queue queue) {
         return new QueueResponse(queue.getToken(), queue.getKey().toString(), queue.getWaitingNumber());
     }
-
 }
