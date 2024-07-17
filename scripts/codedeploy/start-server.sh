079105ee81a8098bbd9c05d492fd1a49476cd03b
WonhoLeeDev <wonholee.dev@gmail.com>
2024-07-17 18:01:44 +0900

    :rocket: jar 파일 명 변경

--- scripts/codedeploy/start-server.sh
+++ scripts/codedeploy/start-server.sh
@@ -4,5 +4,5 @@
 echo "--------------- 서버 배포 시작 -----------------"
 cd /home/ubuntu/concert-booking-server
 sudo fuser -k -n tcp 8080 || true
-nohup java -jar project.jar > ./output.log 2>&1 &
+nohup java -jar concert-booking.jar > ./output.log 2>&1 &
 echo "--------------- 서버 배포 끝 -----------------"
