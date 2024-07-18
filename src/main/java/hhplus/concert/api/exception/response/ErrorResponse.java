3876918454e0bec2a19f42e9f2efd9ad1194fd52
WonhoLeeDev <wonholee.dev@gmail.com>
2024-07-18 21:35:24 +0900

    :recycle: GlobalExceptionHandler 리팩토링

--- src/main/java/hhplus/concert/api/exception/response/ErrorResponse.java
+++ src/main/java/hhplus/concert/api/exception/response/ErrorResponse.java
@@ -1,36 +1,9 @@
 package hhplus.concert.api.exception.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.validation.FieldError;
+import hhplus.concert.api.exception.code.ErrorCode;
 
-import java.util.List;
-
-@Getter
-@Builder
-@RequiredArgsConstructor
-public class ErrorResponse {
-    private final String code;
-    private final String message;
-
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    private final List<ValidationError> errors;
-
-    @Getter
-    @Builder
-    @RequiredArgsConstructor
-    public static class ValidationError {
-
-        private final String field;
-        private final String message;
-
-        public static ValidationError of(final FieldError fieldError) {
-            return ValidationError.builder()
-                    .field(fieldError.getField())
-                    .message(fieldError.getDefaultMessage())
-                    .build();
-        }
+public record ErrorResponse(String code, String message) {
+    public static ErrorResponse from(ErrorCode errorCode) {
+        return new ErrorResponse(errorCode.name(), errorCode.getMessage());
     }
 }
