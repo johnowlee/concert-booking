8aaf5e157f94f8f30f11787c03d1a6b3ca0130d9
johnowlee <johnowlee@gmail.com>
2024-08-06 01:35:36 +0900

    :ambulance: ErrorResponse 반환 객체 수정

--- src/main/java/hhplus/concert/api/exception/response/ErrorResponse.java
+++ src/main/java/hhplus/concert/api/exception/response/ErrorResponse.java
@@ -1,17 +1,23 @@
 package hhplus.concert.api.exception.response;
 
 import hhplus.concert.api.exception.code.ErrorCode;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 
-public record ErrorResponse(String code, String message) {
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+public record ErrorResponse(int status, String code, String message) {
     public static ErrorResponse from(ErrorCode errorCode) {
-        return new ErrorResponse(errorCode.name(), errorCode.getMessage());
+        return new ErrorResponse(
+                errorCode.getHttpStatus().value(),
+                errorCode.name(),
+                errorCode.getMessage()
+        );
     }
 
     public static ErrorResponse from(BindException e) {
         return new ErrorResponse(
-                HttpStatus.BAD_REQUEST.name(),
+                BAD_REQUEST.value(),
+                BAD_REQUEST.name(),
                 e.getBindingResult().getAllErrors().get(0).getDefaultMessage()
         );
     }
