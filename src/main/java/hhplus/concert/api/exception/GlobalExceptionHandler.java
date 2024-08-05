b511e485030f8f322b41c36facdd376d8b55dc1d
johnowlee <johnowlee@gmail.com>
2024-08-05 17:43:02 +0900

    :sparkles: BindException 예외처리

--- src/main/java/hhplus/concert/api/exception/GlobalExceptionHandler.java
+++ src/main/java/hhplus/concert/api/exception/GlobalExceptionHandler.java
@@ -3,7 +3,9 @@ package hhplus.concert.api.exception;
 import hhplus.concert.api.exception.code.ErrorCode;
 import hhplus.concert.api.exception.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +16,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(RestApiException.class)
     public ResponseEntity<Object> handleCustomException(RestApiException e) {
         return createErrorResponseEntity(e.getErrorCode());
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<Object> bindException(BindException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse.from(e));
     }
 
     private static ResponseEntity<Object> createErrorResponseEntity(ErrorCode errorCode) {
--- src/main/java/hhplus/concert/api/exception/response/ErrorResponse.java
+++ src/main/java/hhplus/concert/api/exception/response/ErrorResponse.java
@@ -1,9 +1,18 @@
 package hhplus.concert.api.exception.response;
 
 import hhplus.concert.api.exception.code.ErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
 
 public record ErrorResponse(String code, String message) {
     public static ErrorResponse from(ErrorCode errorCode) {
         return new ErrorResponse(errorCode.name(), errorCode.getMessage());
+    }
+
+    public static ErrorResponse from(BindException e) {
+        return new ErrorResponse(
+                HttpStatus.BAD_REQUEST.name(),
+                e.getBindingResult().getAllErrors().get(0).getDefaultMessage()
+        );
     }
 }
