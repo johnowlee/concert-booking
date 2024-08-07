e7b4c2f123fadc2ef230c6a0311b8e53a2d95323
johnowlee <johnowlee@gmail.com>
2024-08-07 23:57:26 +0900

    :recycle: RestApiException 생성자 초기화 수정

--- src/main/java/hhplus/concert/api/exception/RestApiException.java
+++ src/main/java/hhplus/concert/api/exception/RestApiException.java
@@ -9,6 +9,7 @@ public class RestApiException extends RuntimeException {
     private final ErrorCode errorCode;
 
     public RestApiException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
 }
