6569639b87c38f304009229a56da5c0f7904bd4e
johnowlee <johnowlee@gmail.com>
2024-08-09 14:17:47 +0900

    :white_check_mark: BalanceReader 컴포넌트 테스트 재작성

--- src/test/java/hhplus/concert/domain/history/balance/components/BalanceReaderTest.java
+++ src/test/java/hhplus/concert/domain/history/balance/components/BalanceReaderTest.java
@@ -1,44 +1,79 @@
 package hhplus.concert.domain.history.balance.components;
 
-import hhplus.concert.domain.history.balance.components.BalanceReader;
-import hhplus.concert.domain.history.balance.models.TransactionType;
-import hhplus.concert.domain.history.balance.repositories.BalanceReaderRepository;
+import hhplus.concert.IntegrationTestSupport;
+import hhplus.concert.domain.history.balance.infrastructure.BalanceJpaRepository;
+import hhplus.concert.domain.history.balance.models.Balance;
+import hhplus.concert.domain.support.ClockManager;
+import hhplus.concert.domain.user.infrastructure.UserJpaRepository;
+import hhplus.concert.domain.user.models.User;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.times;
+import static hhplus.concert.domain.history.balance.models.TransactionType.CHARGE;
+import static hhplus.concert.domain.history.balance.models.TransactionType.USE;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(MockitoExtension.class)
-class BalanceReaderTest {
+@Transactional
+class BalanceReaderTest extends IntegrationTestSupport {
 
-    @InjectMocks
+    @Autowired
     BalanceReader balanceReader;
 
-    @Mock
-    BalanceReaderRepository balanceReaderRepository;
+    @Autowired
+    BalanceJpaRepository balanceJpaRepository;
 
+    @MockBean
+    ClockManager clockManager;
+
+    @Autowired
+    UserJpaRepository userJpaRepository;
+
+    @DisplayName("transaction type에 따라 balance 거래 내역 합계액이 조회된다.")
     @Test
-    void GetAmountBySearchParam_Success() {
+    void getAmountBySearchParam() {
         // given
-        long expectedAmount = 1000L;
-        given(balanceReaderRepository.getAmountBySearchParam(anyLong(), any(TransactionType.class), any(LocalDateTime.class), any(LocalDateTime.class))).willReturn(expectedAmount);
+        User user = User.builder()
+                .id(1L)
+                .name("jon")
+                .version(1L)
+                .build();
+        User savedUser = userJpaRepository.save(user);
+
+        LocalDateTime transactionDateTime = LocalDateTime.of(2024, 8, 9, 11, 30, 30);
+
+        Balance balance1 = Balance.builder()
+                .user(savedUser)
+                .transactionType(CHARGE)
+                .transactionDateTime(transactionDateTime)
+                .amount(10000L)
+                .build();
+        Balance balance2 = Balance.builder()
+                .user(savedUser)
+                .transactionType(USE)
+                .transactionDateTime(transactionDateTime.plusHours(1))
+                .amount(5000L)
+                .build();
+        Balance balance3 = Balance.builder()
+                .user(savedUser)
+                .transactionType(USE)
+                .transactionDateTime(transactionDateTime.plusHours(2))
+                .amount(2000L)
+                .build();
+
+        balanceJpaRepository.saveAll(List.of(balance1, balance2, balance3));
 
         // when
-        long result = balanceReader.getAmountBySearchParam(1L, TransactionType.USE, LocalDateTime.now().minusDays(10), LocalDateTime.now());
+        long amountBySearchParamWithCharge = balanceReader.getAmountBySearchParam(savedUser.getId(), CHARGE, transactionDateTime, transactionDateTime.plusDays(1));
+        long amountBySearchParamWithUse = balanceReader.getAmountBySearchParam(savedUser.getId(), USE, transactionDateTime, transactionDateTime.plusDays(1));
 
         // then
-        assertEquals(expectedAmount, result);
-        then(balanceReaderRepository).should(times(1)).getAmountBySearchParam(anyLong(), any(TransactionType.class), any(LocalDateTime.class), any(LocalDateTime.class));
+        assertThat(amountBySearchParamWithCharge).isEqualTo(10000L);
+        assertThat(amountBySearchParamWithUse).isEqualTo(5000L + 2000L);
     }
-
 }
