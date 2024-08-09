6f3adc15f20eea4f3df4a8d8584ffd940faae9ce
johnowlee <johnowlee@gmail.com>
2024-08-09 14:11:21 +0900

    :white_check_mark: BalanceCoreReaderRepository 테스트 수정

--- src/test/java/hhplus/concert/domain/history/balance/infrastructure/BalanceCoreReaderRepositoryTest.java
+++ src/test/java/hhplus/concert/domain/history/balance/infrastructure/BalanceCoreReaderRepositoryTest.java
@@ -2,10 +2,12 @@ package hhplus.concert.domain.history.balance.infrastructure;
 
 import hhplus.concert.IntegrationTestSupport;
 import hhplus.concert.domain.history.balance.models.Balance;
+import hhplus.concert.domain.user.infrastructure.UserJpaRepository;
 import hhplus.concert.domain.user.models.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,6 +16,7 @@ import static hhplus.concert.domain.history.balance.models.TransactionType.CHARG
 import static hhplus.concert.domain.history.balance.models.TransactionType.USE;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 class BalanceCoreReaderRepositoryTest extends IntegrationTestSupport {
 
     @Autowired
@@ -21,6 +24,9 @@ class BalanceCoreReaderRepositoryTest extends IntegrationTestSupport {
 
     @Autowired
     BalanceJpaRepository balanceJpaRepository;
+
+    @Autowired
+    UserJpaRepository userJpaRepository;
 
     @DisplayName("transaction type에 따라 balance 거래 내역 합계액이 조회된다.")
     @Test
@@ -31,21 +37,23 @@ class BalanceCoreReaderRepositoryTest extends IntegrationTestSupport {
                 .name("jon")
                 .version(1L)
                 .build();
+        User savedUser = userJpaRepository.save(user);
+
         LocalDateTime transactionDateTime = LocalDateTime.of(2024, 8, 8, 00, 25);
         Balance balance1 = Balance.builder()
-                .user(user)
+                .user(savedUser)
                 .transactionType(CHARGE)
                 .transactionDateTime(transactionDateTime)
                 .amount(10000L)
                 .build();
         Balance balance2 = Balance.builder()
-                .user(user)
+                .user(savedUser)
                 .transactionType(USE)
                 .transactionDateTime(transactionDateTime.plusHours(1))
                 .amount(5000L)
                 .build();
         Balance balance3 = Balance.builder()
-                .user(user)
+                .user(savedUser)
                 .transactionType(USE)
                 .transactionDateTime(transactionDateTime.plusHours(2))
                 .amount(2000L)
@@ -54,8 +62,8 @@ class BalanceCoreReaderRepositoryTest extends IntegrationTestSupport {
         balanceJpaRepository.saveAll(List.of(balance1, balance2, balance3));
 
         // when
-        long amountBySearchParamWithCharge = balanceCoreReaderRepository.getAmountBySearchParam(user.getId(), CHARGE, transactionDateTime, transactionDateTime.plusDays(1));
-        long amountBySearchParamWithUse = balanceCoreReaderRepository.getAmountBySearchParam(user.getId(), USE, transactionDateTime, transactionDateTime.plusDays(1));
+        long amountBySearchParamWithCharge = balanceCoreReaderRepository.getAmountBySearchParam(savedUser.getId(), CHARGE, transactionDateTime, transactionDateTime.plusDays(1));
+        long amountBySearchParamWithUse = balanceCoreReaderRepository.getAmountBySearchParam(savedUser.getId(), USE, transactionDateTime, transactionDateTime.plusDays(1));
 
         // then
         assertThat(amountBySearchParamWithCharge).isEqualTo(10000L);
