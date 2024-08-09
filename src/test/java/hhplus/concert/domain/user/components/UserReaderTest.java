f0737f2c3fe23d09debcb608846ad414d1457262
johnowlee <johnowlee@gmail.com>
2024-08-09 17:10:47 +0900

    :white_check_mark: UserReader 테스트 재작성

--- src/test/java/hhplus/concert/domain/user/components/UserReaderTest.java
+++ src/test/java/hhplus/concert/domain/user/components/UserReaderTest.java
@@ -1,49 +1,56 @@
 package hhplus.concert.domain.user.components;
 
+import hhplus.concert.IntegrationTestSupport;
+import hhplus.concert.api.exception.RestApiException;
+import hhplus.concert.domain.user.infrastructure.UserJpaRepository;
 import hhplus.concert.domain.user.models.User;
-import hhplus.concert.domain.user.repositories.UserReaderRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Optional;
-
+import static hhplus.concert.api.exception.code.UserErrorCode.NOT_FOUND_USER;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@ExtendWith(MockitoExtension.class)
-class UserReaderTest {
+class UserReaderTest extends IntegrationTestSupport {
 
-    @InjectMocks
+    @Autowired
     UserReader userReader;
 
-    @Mock
-    UserReaderRepository userReaderRepository;
+    @Autowired
+    UserJpaRepository userJpaRepository;
 
-    @DisplayName("인자값이 모두 유효하면, 유저 조회에 성공한다.")
+    @DisplayName("user를 id로 조회한다.")
     @Test
-    void getUserById_Success_ifWithValidArguments() {
+    void getUserById() {
         // given
-        Long userId = 1L;
-        String name = "John Doe";
-        User expected = User.builder()
-                .id(userId)
-                .name(name)
+        User user = User.builder()
+                .name("jon")
                 .build();
-
-        given(userReaderRepository.getUserById(userId)).willReturn(Optional.ofNullable(expected));
+        User savedUser = userJpaRepository.save(user);
 
         // when
-        User result = userReader.getUserById(userId);
+        User result = userReader.getUserById(savedUser.getId());
 
         // then
-        assertThat(result.getId()).isEqualTo(expected.getId());
-        assertThat(result.getName()).isEqualTo(expected.getName());
-        verify(userReaderRepository, times(1)).getUserById(userId);
+        assertThat(result).isNotNull();
+        assertThat(result.getId()).isEqualTo(savedUser.getId());
+        assertThat(result.getName()).isEqualTo(savedUser.getName());
+    }
+
+    @DisplayName("user를 id로 조회했을때 결과가 없으면 예외가 발생한다.")
+    @Test
+    void getUserByIdWithNotExistedUser() {
+        // given
+        User user = User.builder()
+                .name("jon")
+                .version(1L)
+                .build();
+        userJpaRepository.save(user);
+
+        // when & then
+        assertThatThrownBy(() -> userReader.getUserById(2L))
+                .isInstanceOf(RestApiException.class)
+                .hasMessage(NOT_FOUND_USER.getMessage());
     }
 }
