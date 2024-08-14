141a4cd90603e934bd7b1975a6444290be53cdde
johnowlee <johnowlee@gmail.com>
2024-08-14 14:11:30 +0900

    :white_check_mark: ChargeBalanceUseCase 테스트 작성

--- src/test/java/hhplus/concert/api/balance/usecase/ChargeBalanceUseCaseTest.java
+++ src/test/java/hhplus/concert/api/balance/usecase/ChargeBalanceUseCaseTest.java
@@ -1,62 +1,129 @@
 package hhplus.concert.api.balance.usecase;
 
+import hhplus.concert.IntegrationTestSupport;
+import hhplus.concert.api.balance.dto.response.BalanceChargeResponse;
+import hhplus.concert.api.common.ResponseResult;
+import hhplus.concert.api.exception.RestApiException;
 import hhplus.concert.domain.history.balance.components.BalanceWriter;
 import hhplus.concert.domain.user.components.UserReader;
 import hhplus.concert.domain.user.components.UserWriter;
 import hhplus.concert.domain.user.models.User;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.IntStream;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static hhplus.concert.api.exception.code.UserErrorCode.NOT_FOUND_USER;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@SpringBootTest
-class ChargeBalanceUseCaseTest {
+class ChargeBalanceUseCaseTest extends IntegrationTestSupport {
 
     @Autowired
     ChargeBalanceUseCase chargeBalanceUseCase;
+
     @Autowired
     UserReader userReader;
+
     @Autowired
     UserWriter userWriter;
+
     @Autowired
     BalanceWriter balanceWriter;
+
+    @Autowired
+    EntityManager em;
 
     @BeforeEach
     void setUp() {
 
     }
 
-    @DisplayName("포인트 충전이 동시에 발생하면, 예외를 반환하고 한 건만 충전에 성공한다.")
+    @DisplayName("유저의 잔액을 충전한다.")
+    @Transactional
     @Test
-    void chargeBalanceJustOneCase_Success_ifConcurrencyOccurs() {
-        // TODO: Task 횟수 더 늘려서 테스트 다시 수행
-        //given
-        User savedUser = userWriter.save(User.builder().build());
-        final User user = userReader.getUserById(savedUser.getId());
-        int numOfTask = 5;
+    void execute() {
+        // given
+        User savingUser = User.builder()
+                .name("jon")
+                .balance(10000L)
+                .build();
+        User savedUser = userWriter.save(savingUser);
 
-        //when
-        CompletableFuture<?>[] futures = IntStream.range(0, numOfTask)
-                .mapToObj(i -> CompletableFuture.runAsync(() -> {
-                    try {
-                        chargeBalanceUseCase.execute(user.getId(), 10000);
-                    } catch (Exception e) {
-                    }
-                }))
-                .toArray(CompletableFuture[]::new);
+        User user = userReader.getUserById(savedUser.getId());
+        long balance = user.getBalance();
+        long version = user.getVersion();
 
-        CompletableFuture.allOf(futures).join();  // 모든 비동기 작업이 끝날 때까지 기다림
+        // when
+        BalanceChargeResponse result = chargeBalanceUseCase.execute(user.getId(), 30000L);
 
-        //then
-        User UpdatedUser = userReader.getUserById(user.getId());
+        // then
+        assertThat(user.getVersion()).isEqualTo(version + 1);
+        assertThat(result.chargeResult()).isEqualTo(ResponseResult.SUCCESS);
+        assertThat(result.balance()).isEqualTo(balance + 30000L);
+    }
 
-        assertEquals(user.getVersion() + 1, UpdatedUser.getVersion());
-        assertEquals(user.getBalance() + 10000, UpdatedUser.getBalance());
+    @DisplayName("등록되지 않은 유저의 잔액 충전 시 예외가 발생한다.")
+    @Test
+    void executeWithNotFoundUser() {
+        // given
+        User savingUser = User.builder()
+                .name("jon")
+                .balance(10000L)
+                .build();
+        User savedUser = userWriter.save(savingUser);
+
+        User user = userReader.getUserById(savedUser.getId());
+        Long userId = user.getId();
+        Long notFoundUserId = 20L;
+
+        // when & then
+        assertThat(userId).isNotEqualTo(notFoundUserId);
+        assertThatThrownBy(() -> chargeBalanceUseCase.execute(notFoundUserId, 30000L))
+                .isInstanceOf(RestApiException.class)
+                .hasMessage(NOT_FOUND_USER.getMessage());
+    }
+
+    @DisplayName("동시에 잔액충전이 발생하면 낙관적락에 의해 1건만 수행된다.")
+    @Test
+    void executeConcurrently() throws InterruptedException {
+        // given
+        User savingUser = User.builder()
+                .name("jon")
+                .balance(10000L)
+                .build();
+        User savedUser = userWriter.save(savingUser);
+
+        User user = userReader.getUserById(savedUser.getId());
+        Long userId = user.getId();
+        long chargeAmount = 30000L;
+
+        int threadCount = 5;
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    chargeBalanceUseCase.execute(userId, chargeAmount);
+                } catch (RestApiException e) {
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();  // 모든 스레드가 작업을 마칠 때까지 대기
+        executorService.shutdown(); // 스레드 풀이 더 이상 작업을 기다리지 않도록 종료
+
+        // then
+        User result = userReader.getUserById(userId);
+        assertThat(result.getBalance()).isEqualTo(10000L + 30000L);
     }
 }
