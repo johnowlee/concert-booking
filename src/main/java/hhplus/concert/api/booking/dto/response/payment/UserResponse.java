8197d3a484352f81f5c652b4edbd26a0966036b5
johnowlee <johnowlee@gmail.com>
2024-08-19 19:38:41 +0900

    :sparkles: UserResponse 생성

---
+++ src/main/java/hhplus/concert/api/booking/dto/response/payment/UserResponse.java
@@ -0,0 +1,18 @@
+package hhplus.concert.api.booking.dto.response.payment;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import hhplus.concert.domain.booking.models.Booking;
+import hhplus.concert.domain.history.payment.models.Payment;
+
+import java.util.List;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@JsonInclude(NON_NULL)
+public record UserResponse(
+        Long id,
+        String name,
+        Long balance,
+        List<Booking> bookings,
+        List<Payment> payments
+) {}
