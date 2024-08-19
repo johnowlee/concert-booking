e887d33e32a706a3292f821ba440c980976c0f1e
johnowlee <johnowlee@gmail.com>
2024-08-19 19:39:05 +0900

    :recycle: PaymentResponse 리팩토링

--- src/main/java/hhplus/concert/api/booking/dto/response/payment/PaymentResponse.java
+++ src/main/java/hhplus/concert/api/booking/dto/response/payment/PaymentResponse.java
@@ -1,9 +1,40 @@
 package hhplus.concert.api.booking.dto.response.payment;
 
-import hhplus.concert.api.common.ResponseResult;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import hhplus.concert.domain.booking.models.Booking;
+import hhplus.concert.domain.history.payment.models.Payment;
+import hhplus.concert.domain.user.models.User;
 
-public record PaymentResponse(ResponseResult paymentResult) {
-    public static PaymentResponse from(ResponseResult paymentResult) {
-        return new PaymentResponse(paymentResult);
+import java.time.LocalDateTime;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@JsonInclude(NON_NULL)
+public record PaymentResponse(
+        Long id,
+        LocalDateTime paymentDateTime,
+        Integer paymentAmount,
+        UserResponse user,
+        BookingResponse booking
+) {
+
+    public static PaymentResponse from(Payment payment) {
+        User user = payment.getUser();
+        Booking booking = payment.getBooking();
+        return new PaymentResponse(
+                payment.getId(),
+                payment.getPaymentDateTime(),
+                payment.getPaymentAmount(),
+                createUserResponse(user),
+                createBookingResponse(booking)
+        );
+    }
+
+    private static UserResponse createUserResponse(User user) {
+        return new UserResponse(user.getId(), user.getName(), null, null, null);
+    }
+
+    private static BookingResponse createBookingResponse(Booking booking) {
+        return new BookingResponse(booking.getId(), booking.getBookingStatus(), null, booking.getConcertTitle(), null, null);
     }
 }
