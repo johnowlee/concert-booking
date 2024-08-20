3a5fb0dc626baef0287e66d2d5326f86188ad905
johnowlee <johnowlee@gmail.com>
2024-08-20 18:54:17 +0900

    :fire: 미사용 클래스 삭제

--- src/main/java/hhplus/concert/api/balance/usecase/response/BalanceChargeResponse.java
+++
@@ -1,9 +0,0 @@
-package hhplus.concert.api.balance.usecase.response;
-
-import hhplus.concert.api.common.ResponseResult;
-
-public record BalanceChargeResponse(ResponseResult chargeResult, long balance) {
-    public static BalanceChargeResponse success(long balance) {
-        return new BalanceChargeResponse(ResponseResult.SUCCESS, balance);
-    }
-}
