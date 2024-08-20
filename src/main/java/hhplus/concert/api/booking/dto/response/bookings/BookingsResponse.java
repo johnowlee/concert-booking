6c88d625fa5f73f0a0d00b0837d4e5c566bdabd9
johnowlee <johnowlee@gmail.com>
2024-08-20 23:50:54 +0900

    :label: GetBookingsByUserIdUseCase Response 타입 변경

--- src/main/java/hhplus/concert/api/booking/dto/response/bookings/BookingsResponse.java
+++ src/main/java/hhplus/concert/api/booking/dto/response/bookings/BookingsResponse.java
@@ -1,26 +1,18 @@
 package hhplus.concert.api.booking.dto.response.bookings;
 
+import hhplus.concert.api.common.response.BookingResponse;
 import hhplus.concert.domain.booking.models.Booking;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public record BookingsResponse(List<BookingsDto> bookings) {
+public record BookingsResponse(List<BookingResponse> bookings) {
 
     public static BookingsResponse from(List<Booking> bookings) {
-        return new BookingsResponse(convertBookingsDtoList(bookings));
-    }
-
-    private static List<BookingsDto> convertBookingsDtoList(List<Booking> bookings) {
-        return bookings.stream()
-                .map(b -> toBookingsDto(b))
-                .collect(Collectors.toList());
-    }
-
-    private static BookingsDto toBookingsDto(Booking booking) {
-        return BookingsDto.of(booking.getId(),
-                booking.getBookingStatus(),
-                booking.getBookingDateTime(),
-                booking.getConcertTitle());
+        return new BookingsResponse(
+                bookings.stream()
+                    .map(booking -> BookingResponse.from(booking))
+                    .collect(Collectors.toList())
+        );
     }
 }
--- src/test/java/hhplus/concert/api/booking/usecase/GetBookingsByUserIdUseCaseTest.java
+++ src/test/java/hhplus/concert/api/booking/usecase/GetBookingsByUserIdUseCaseTest.java
@@ -3,6 +3,10 @@ package hhplus.concert.api.booking.usecase;
 import hhplus.concert.api.booking.dto.response.bookings.BookingsResponse;
 import hhplus.concert.domain.booking.components.BookingReader;
 import hhplus.concert.domain.booking.models.Booking;
+import hhplus.concert.domain.booking.models.BookingSeat;
+import hhplus.concert.domain.concert.models.ConcertOption;
+import hhplus.concert.domain.concert.models.Seat;
+import hhplus.concert.domain.user.models.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,7 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static hhplus.concert.domain.booking.models.BookingStatus.*;
+import static hhplus.concert.domain.booking.models.BookingStatus.COMPLETE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 import static org.mockito.BDDMockito.given;
@@ -32,14 +36,22 @@ class GetBookingsByUserIdUseCaseTest {
     @Test
     void execute() {
         // given
-        Booking booking = mock(Booking.class);
-
         LocalDateTime bookingDateTime = LocalDateTime.of(2024, 8, 10, 12, 00);
-
+        Booking booking = mock(Booking.class);
         given(booking.getId()).willReturn(1L);
         given(booking.getBookingStatus()).willReturn(COMPLETE);
         given(booking.getBookingDateTime()).willReturn(bookingDateTime);
         given(booking.getConcertTitle()).willReturn("아이유 콘서트");
+        given(booking.getUser()).willReturn(mock(User.class));
+
+        BookingSeat bookingSeat = mock(BookingSeat.class);
+        given(booking.getBookingSeats()).willReturn(List.of(bookingSeat));
+
+        Seat seat = mock(Seat.class);
+        given(bookingSeat.getSeat()).willReturn(seat);
+
+        ConcertOption concertOption = mock(ConcertOption.class);
+        given(seat.getConcertOption()).willReturn(concertOption);
 
         given(bookingReader.getBookingsByUserId(1L)).willReturn(List.of(booking));
 
@@ -48,7 +60,7 @@ class GetBookingsByUserIdUseCaseTest {
 
         // then
         assertThat(result.bookings()).hasSize(1)
-                .extracting("bookingId", "bookingStatus", "bookingDateTime", "title")
+                .extracting("id", "bookingStatus", "bookingDateTime", "concertTitle")
                 .contains(
                         tuple(1L, COMPLETE, bookingDateTime, "아이유 콘서트")
                 );
