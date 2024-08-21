a2debea6df0c528ec9146e77a904ec728669f8d5
johnowlee <johnowlee@gmail.com>
2024-08-21 20:09:55 +0900

    :ambulance: equals 재정의 오류 수정

--- src/main/java/hhplus/concert/domain/user/models/User.java
+++ src/main/java/hhplus/concert/domain/user/models/User.java
@@ -69,14 +69,17 @@ public class User {
 
     @Override
     public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        User user = (User) object;
-        return Objects.equals(id, user.id);
+        if (this == object){
+            return true;
+        }
+        if (!(object instanceof User user)) {
+            return false;
+        }
+        return this.getId() != null && Objects.equals(getId(), user.getId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(getId());
     }
 }
