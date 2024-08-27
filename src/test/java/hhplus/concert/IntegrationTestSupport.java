1614b16d9c2304fa74cfcc9429bef40674b093cc
johnowlee <johnowlee@gmail.com>
2024-08-27 18:08:16 +0900

    :recycle: IntegrationTestSupport에서 영속데이터 명시적 초기화

--- src/test/java/hhplus/concert/IntegrationTestSupport.java
+++ src/test/java/hhplus/concert/IntegrationTestSupport.java
@@ -1,14 +1,22 @@
 package hhplus.concert;
 
+import hhplus.concert.domain.booking.infrastructure.BookingJpaRepository;
+import hhplus.concert.domain.booking.infrastructure.BookingSeatJpaRepository;
+import hhplus.concert.domain.concert.infrastructure.ConcertJpaRepository;
+import hhplus.concert.domain.concert.infrastructure.ConcertOptionJpaRepository;
+import hhplus.concert.domain.concert.infrastructure.SeatJpaRepository;
+import hhplus.concert.domain.history.balance.infrastructure.BalanceJpaRepository;
+import hhplus.concert.domain.history.payment.infrastructure.PaymentJpaRepository;
+import hhplus.concert.domain.user.infrastructure.UserJpaRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@DirtiesContext
 @SpringBootTest
 public abstract class IntegrationTestSupport {
 
@@ -17,4 +25,44 @@ public abstract class IntegrationTestSupport {
 
     @MockBean
     protected RedisMessageListenerContainer redisContainer;
+
+
+    @Autowired
+    BookingSeatJpaRepository bookingSeatJpaRepository;
+
+    @Autowired
+    SeatJpaRepository seatJpaRepository;
+
+    @Autowired
+    ConcertOptionJpaRepository concertOptionJpaRepository;
+
+    @Autowired
+    ConcertJpaRepository concertJpaRepository;
+
+
+    @Autowired
+    BalanceJpaRepository balanceJpaRepository;
+
+    @Autowired
+    PaymentJpaRepository paymentJpaRepository;
+
+    @Autowired
+    BookingJpaRepository bookingJpaRepository;
+
+    @Autowired
+    UserJpaRepository userJpaRepository;
+
+
+    @AfterEach
+    void tearDown() {
+        bookingSeatJpaRepository.deleteAllInBatch();
+        seatJpaRepository.deleteAllInBatch();
+        concertOptionJpaRepository.deleteAllInBatch();
+        concertJpaRepository.deleteAllInBatch();
+
+        balanceJpaRepository.deleteAllInBatch();
+        paymentJpaRepository.deleteAllInBatch();
+        bookingJpaRepository.deleteAllInBatch();
+        userJpaRepository.deleteAllInBatch();
+    }
 }
