70588bcc9962f0cb9e8b2db7363d3b0bbf1918d4
johnowlee <johnowlee@gmail.com>
2024-08-27 18:58:04 +0900

    :recycle: 예약 상태 검증 로직 리팩토링

--- src/main/java/hhplus/concert/domain/booking/support/BookingValidator.java
+++ src/main/java/hhplus/concert/domain/booking/support/BookingValidator.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static hhplus.concert.api.exception.code.BookingErrorCode.ALREADY_BOOKED;
 import static hhplus.concert.api.exception.code.BookingErrorCode.PENDING_BOOKING;
+import static hhplus.concert.domain.booking.models.BookingStatus.COMPLETE;
 import static hhplus.concert.domain.history.payment.models.PaymentTimeLimitPolicy.ALLOWED_MINUTES;
 
 @Slf4j
@@ -16,11 +18,17 @@ import static hhplus.concert.domain.history.payment.models.PaymentTimeLimitPolic
 public class BookingValidator {
 
     public void checkAnyAlreadyCompleteBooking(List<Booking> bookings) {
-        bookings.forEach(Booking::validateAlreadyBooked);
+        bookings.forEach(BookingValidator::validateAlreadyBooked);
     }
 
     public void checkAnyPendingBooking(List<Booking> bookings, LocalDateTime localDateTime) {
         bookings.forEach(booking -> validatePendingBooking(booking, localDateTime));
+    }
+
+    private static void validateAlreadyBooked(Booking booking) {
+        if (booking.getBookingStatus() == COMPLETE) {
+            throw new RestApiException(ALREADY_BOOKED);
+        }
     }
 
     private static void validatePendingBooking(Booking booking, LocalDateTime verificationTime) {
