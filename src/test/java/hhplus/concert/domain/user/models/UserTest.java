84bef81724bdbe0c433117bbb0045315cc03e913
johnowlee <johnowlee@gmail.com>
2024-08-27 20:59:40 +0900

    :white_check_mark: 추가된 메서드 테스트 작성

--- src/test/java/hhplus/concert/domain/user/models/UserTest.java
+++ src/test/java/hhplus/concert/domain/user/models/UserTest.java
@@ -1,6 +1,7 @@
 package hhplus.concert.domain.user.models;
 
 import hhplus.concert.api.exception.RestApiException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -50,5 +51,40 @@ class UserTest {
         assertThatThrownBy(() -> user.useBalance(useAmount))
                 .isInstanceOf(RestApiException.class)
                 .hasMessage(NOT_ENOUGH_BALANCE.getMessage());
+    }
+
+    @DisplayName("잔액이 비교액 미만이면 true를 반환한다.")
+    @Test
+    void isBalanceLessThan() {
+        // given
+        long targetAmount = 20000L;
+        User user = User.builder()
+                .balance(10000L)
+                .build();
+
+        // when
+        boolean result = user.isBalanceLessThan(targetAmount);
+
+        // then
+        Assertions.assertThat(result).isTrue();
+    }
+
+    @DisplayName("잔액이 비교액 이상이면 false를 반환한다.")
+    @Test
+    void isBalanceNotLessThan() {
+        // given
+        long equalAmount = 10000L;
+        long greaterAmount = 5000L;
+        User user = User.builder()
+                .balance(10000L)
+                .build();
+
+        // when
+        boolean equalAmountResult = user.isBalanceLessThan(equalAmount);
+        boolean greaterAmountResult = user.isBalanceLessThan(greaterAmount);
+
+        // then
+        Assertions.assertThat(equalAmountResult).isFalse();
+        Assertions.assertThat(greaterAmountResult).isFalse();
     }
 }
