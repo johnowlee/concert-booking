24be53a560ad22e212d5bb3bf5d9e17291a7b8b6
johnowlee <johnowlee@gmail.com>
2024-08-27 18:50:21 +0900

    :recycle: 예약 가능 검증 로직 리팩토링

--- src/main/java/hhplus/concert/domain/booking/support/BookingValidator.java
+++ src/main/java/hhplus/concert/domain/booking/support/BookingValidator.java
@@ -1,5 +1,6 @@
 package hhplus.concert.domain.booking.support;
 
+import hhplus.concert.api.exception.RestApiException;
 import hhplus.concert.domain.booking.models.Booking;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -7,15 +8,29 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static hhplus.concert.api.exception.code.BookingErrorCode.PENDING_BOOKING;
+import static hhplus.concert.domain.history.payment.models.PaymentTimeLimitPolicy.ALLOWED_MINUTES;
+
 @Slf4j
 @Component
 public class BookingValidator {
 
     public void checkAnyAlreadyCompleteBooking(List<Booking> bookings) {
-        bookings.forEach(booking -> booking.validateAlreadyBooked());
+        bookings.forEach(Booking::validateAlreadyBooked);
     }
 
     public void checkAnyPendingBooking(List<Booking> bookings, LocalDateTime localDateTime) {
-        bookings.forEach(booking -> booking.validatePendingBooking(localDateTime));
+        bookings.forEach(booking -> validatePendingBooking(booking, localDateTime));
+    }
+
+    private static void validatePendingBooking(Booking booking, LocalDateTime verificationTime) {
+        long passedMinutes = booking.getPassedMinutesSinceBookingFrom(verificationTime);
+        if (isPayableTimeNotOver(passedMinutes)) {
+            throw new RestApiException(PENDING_BOOKING);
+        }
+    }
+
+    private static boolean isPayableTimeNotOver(long passedMinutes) {
+        return passedMinutes < ALLOWED_MINUTES.getMinutes();
     }
 }
