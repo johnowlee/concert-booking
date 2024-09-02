cfab42ce48931cad673cd797aa819b798f2c4373
johnowlee <johnowlee@gmail.com>
2024-09-02 13:46:04 +0900

    :sparkles: Balance Service 생성 & 낙관적락 충전 메서드 생성

---
+++ src/main/java/hhplus/concert/domain/history/balance/support/BalanceService.java
@@ -0,0 +1,36 @@
+package hhplus.concert.domain.history.balance.support;
+
+import hhplus.concert.api.common.response.UserResponse;
+import hhplus.concert.api.exception.RestApiException;
+import hhplus.concert.api.exception.code.BalanceErrorCode;
+import hhplus.concert.domain.history.balance.components.BalanceHistoryWriter;
+import hhplus.concert.domain.history.balance.models.Balance;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.OptimisticLockException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BalanceService {
+
+    private final BalanceHistoryWriter balanceHistoryWriter;
+    private final EntityManager em;
+
+    public Balance chargeBalanceWithOptimisticLock(Balance balance) {
+        try {
+            chargeBalance(balance);
+
+            balanceHistoryWriter.save(balance);
+            return balance;
+        } catch (OptimisticLockException e) {
+            throw new RestApiException(BalanceErrorCode.FAILED_CHARGE);
+        }
+    }
+
+    private void chargeBalance(Balance balance) {
+        long chargeAmount = balance.getAmount();
+        balance.getUser().chargeBalance(chargeAmount);
+        em.flush();
+    }
+}
