3854c02f50933187f43caf6534c78e7567d08be7
johnowlee <johnowlee@gmail.com>
2024-09-04 13:37:12 +0900

    :adhesive_bandage: PaymentCompletion 필드명 변경2

--- src/main/java/hhplus/concert/domain/history/payment/event/PaymentCompletion.java
+++ src/main/java/hhplus/concert/domain/history/payment/event/PaymentCompletion.java
@@ -3,10 +3,10 @@ package hhplus.concert.domain.history.payment.event;
 import hhplus.concert.domain.booking.models.Booking;
 import hhplus.concert.domain.history.payment.models.Payment;
 
-public record PaymentCompletion(long userId, long bookingId, long amount) {
+public record PaymentCompletion(long payerId, long bookingId, long paymentAmount) {
 
     public static PaymentCompletion from(Payment payment) {
         Booking booking = payment.getBooking();
-        return new PaymentCompletion(booking.getUser().getId(), booking.getId(), booking.getTotalPrice());
+        return new PaymentCompletion(payment.getUser().getId(), booking.getId(), booking.getTotalPrice());
     }
 }
